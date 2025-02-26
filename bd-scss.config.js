69c0813213642535c743decef684b0257ed801ec
Riddim_GLiTCH <riddim-glitch@proton.me>
2025-02-26 13:22:22 +0100

    Initial Commit - Push Base Version

---
+++ bd-scss.config.js
@@ -0,0 +1,10 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+	meta: {
+		name: "Discord Refresh",
+		author: "Riddim-GLiTCH",
+		version: "1.0.0",
+		description: "An enhanced experience of the Modern Discord Layout.",
+		source: "https://github.com/Riddim-GLiTCH/Discord Refresh"
+	}
+}
