d7c5aada78f115772f16d7f4bfe94135cee7b910
Riddim-GLiTCH <riddim-glitch@proton.me>
2025-02-26 14:26:08 +0100

    Moving repo

--- bd-scss.config.js
+++ bd-scss.config.js
@@ -1,10 +1,10 @@
 /** @type {import('bd-scss/lib/config').Config} */
 export default {
 	meta: {
-		name: "Discord Refresh",
-		author: "Riddim-GLiTCH",
+		name: "Discord-Refresh",
+		author: "Nexus-Themes",
 		version: "1.0.0",
 		description: "An enhanced experience of the Modern Discord Layout.",
-		source: "https://github.com/Riddim-GLiTCH/Discord Refresh"
+		source: "https://github.com/Nexus-Themes/Discord Refresh"
 	}
 }
